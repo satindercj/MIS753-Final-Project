fdfcaab0f87c65a81510fdff920a9422da2f4e20
satindercj <satindercj@gmail.com>
2021-05-04 23:15:25 -0700

    Finished Site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
-import { MatDividerModule } from '@angular/material/divider'
-import { MatCardModule } from '@angular/material/card'
-import { MatListModule } from '@angular/material/list'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatAutocompleteModule } from '@angular/material/autocomplete'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { NgApexchartsModule } from 'ng-apexcharts'
-import { NgxUiLoaderModule } from 'ngx-ui-loader'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { NgxUiLoaderModule } from "ngx-ui-loader";
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,15 +17,9 @@ import { NgxUiLoaderModule } from 'ngx-ui-loader'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatDividerModule,
-    MatCardModule,
-    MatListModule,
     MatFormFieldModule,
     MatInputModule,
     MatAutocompleteModule,
-    MatProgressSpinnerModule,
     FormsModule,
     ReactiveFormsModule,
     NgApexchartsModule,
